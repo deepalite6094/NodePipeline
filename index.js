a576ebbe1c9bda9d7060255713d340d59a9727d0
deepfyniti6094 <deep@aliteprojects.com>
2022-03-26 09:48:17 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const express = require('express')
+const app = express();
+const port = process.env.PORT
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/", (req, res) => {
+  res.send("<h1>Fyniti Iq Payment running</h1>")
+})
+
+const httpServer = http.createServer(app);
+httpServer.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+});
